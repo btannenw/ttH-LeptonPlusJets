550434b916191685c44f2e1d652d9be17a0cbe52
Satoshi Hasegawa <satoshi@fnal.gov>
2017-01-24 03:56:22 -0600

    crab jobsubmit script updated to Moriond17 analysis.

--- YggdrasilTreeMaker/test/make_crabconfigs_80x_Modirond16.sh
+++ YggdrasilTreeMaker/test/make_crabconfigs_80x_Modirond16.sh
@@ -15,11 +15,13 @@ diff yggdrasil_treeMaker_cfg.py __yggdrasil_treeMaker_MC_cfg.py
 cat yggdrasil_treeMaker_cfg.py | sed "s|isMC=False|isMC=True|g" | sed "s|isTTBARMC=False|isTTBARMC=True|g" > __yggdrasil_treeMaker_MCTTBAR_cfg.py 
 diff yggdrasil_treeMaker_cfg.py __yggdrasil_treeMaker_MCTTBAR_cfg.py
 
-cat yggdrasil_treeMaker_cfg.py | sed "s|isMC=True|isMC=False|g" > __yggdrasil_treeMaker_DATA_cfg.py 
-diff yggdrasil_treeMaker_cfg.py __yggdrasil_treeMaker_DATA_cfg.py
 
-nickname="Satoshi_Moriond17_Yggdra_20170117_submit2"
+cat yggdrasil_treeMaker_cfg.py | sed "s|isMC=True|isMC=False|g" | sed "s|isPeriodBCD=False|isPeriodBCD=True|g"> __yggdrasil_treeMaker_DATA_BCD_cfg.py 
+cat yggdrasil_treeMaker_cfg.py | sed "s|isMC=True|isMC=False|g" | sed "s|isPeriodEF1=False|isPeriodEF1=True|g"> __yggdrasil_treeMaker_DATA_EF1_cfg.py 
+cat yggdrasil_treeMaker_cfg.py | sed "s|isMC=True|isMC=False|g" | sed "s|isPeriodF2G=False|isPeriodF2G=True|g"> __yggdrasil_treeMaker_DATA_F2G_cfg.py 
+cat yggdrasil_treeMaker_cfg.py | sed "s|isMC=True|isMC=False|g" | sed "s|isPeriodH=False|isPeriodH=True|g"    > __yggdrasil_treeMaker_DATA_H_cfg.py 
 
+nickname="Satoshi_Moriond17_Yggdra_20170119_submit2"
 
 JobIndexList=""
 
@@ -27,59 +29,71 @@ ds[1]=/SingleElectron/Run2016B-23Sep2016-v3/MINIAOD
 ds[2]=/SingleElectron/Run2016C-23Sep2016-v1/MINIAOD
 ds[3]=/SingleElectron/Run2016D-23Sep2016-v1/MINIAOD
 ds[4]=/SingleElectron/Run2016E-23Sep2016-v1/MINIAOD
+
 ds[5]=/SingleElectron/Run2016F-23Sep2016-v1/MINIAOD
-ds[6]=/SingleElectron/Run2016G-23Sep2016-v1/MINIAOD
-ds[7]=/SingleElectron/Run2016H-PromptReco-v2/MINIAOD
-ds[8]=/SingleElectron/Run2016H-PromptReco-v3/MINIAOD
+ds[6]=/SingleElectron/Run2016F-23Sep2016-v1/MINIAOD
+# |_ Jobs for Period F twice, on purpsoe
+
+ds[7]=/SingleElectron/Run2016G-23Sep2016-v1/MINIAOD
+ds[8]=/SingleElectron/Run2016H-PromptReco-v2/MINIAOD
+ds[9]=/SingleElectron/Run2016H-PromptReco-v3/MINIAOD
 
 
 name[1]=DataElB
 name[2]=DataElC
 name[3]=DataElD
 name[4]=DataElE
-name[5]=DataElF
-name[6]=DataElG
-name[7]=DataElHv2
-name[8]=DataElHv3
-ismc[1]=DATA
-ismc[2]=DATA
-ismc[3]=DATA
-ismc[4]=DATA
-ismc[5]=DATA
-ismc[6]=DATA
-ismc[7]=DATA
-ismc[8]=DATA
+name[5]=DataElF1
+name[6]=DataElF2
+name[7]=DataElG
+name[8]=DataElHv2
+name[9]=DataElHv3
 
-JobIndexList=${JobIndexList}" 1 2 3 4 5 6 7 8 "
+ismc[1]=DATA_BCD
+ismc[2]=DATA_BCD
+ismc[3]=DATA_BCD
+ismc[4]=DATA_EF1
+ismc[5]=DATA_EF1
+ismc[6]=DATA_F2G
+ismc[7]=DATA_F2G
+ismc[8]=DATA_H
+ismc[9]=DATA_H
+
+JobIndexList=${JobIndexList}" 1 2 3 4 5 6 7 8 9 "
 
 ds[11]=/SingleMuon/Run2016B-23Sep2016-v3/MINIAOD
 ds[12]=/SingleMuon/Run2016C-23Sep2016-v1/MINIAOD
 ds[13]=/SingleMuon/Run2016D-23Sep2016-v1/MINIAOD
 ds[14]=/SingleMuon/Run2016E-23Sep2016-v1/MINIAOD
+
 ds[15]=/SingleMuon/Run2016F-23Sep2016-v1/MINIAOD
-ds[16]=/SingleMuon/Run2016G-23Sep2016-v1/MINIAOD
-ds[17]=/SingleMuon/Run2016H-PromptReco-v2/MINIAOD
-ds[18]=/SingleMuon/Run2016H-PromptReco-v3/MINIAOD
+ds[16]=/SingleMuon/Run2016F-23Sep2016-v1/MINIAOD
+# Period F twiwce.
+ds[17]=/SingleMuon/Run2016G-23Sep2016-v1/MINIAOD
+ds[18]=/SingleMuon/Run2016H-PromptReco-v2/MINIAOD
+ds[19]=/SingleMuon/Run2016H-PromptReco-v3/MINIAOD
 
 name[11]=DataMuB
 name[12]=DataMuC
 name[13]=DataMuD
 name[14]=DataMuE
-name[15]=DataMuF
-name[16]=DataMuG
-name[17]=DataMuHv2
-name[18]=DataMuHv3
-ismc[11]=DATA
-ismc[12]=DATA
-ismc[13]=DATA
-ismc[14]=DATA
-ismc[15]=DATA
-ismc[16]=DATA
-ismc[17]=DATA
-ismc[18]=DATA
+name[15]=DataMuF1
+name[16]=DataMuF2
+name[17]=DataMuG
+name[18]=DataMuHv2
+name[19]=DataMuHv3
 
-JobIndexList=${JobIndexList}" 11 12 13 14 15 16 17 18 "
+ismc[11]=DATA_BCD
+ismc[12]=DATA_BCD
+ismc[13]=DATA_BCD
+ismc[14]=DATA_EF1
+ismc[15]=DATA_EF1
+ismc[16]=DATA_F2G
+ismc[17]=DATA_F2G
+ismc[18]=DATA_H
+ismc[19]=DATA_H
 
+JobIndexList=${JobIndexList}" 11 12 13 14 15 16 17 18 19"
 
 
 ds[101]=/privateMCProductionAODSIMMiniAOD/friese-eventAODSIMMiniAOD-TTToSemiLepton_hvq_ttHtranche3-KIT10-28028af67189b3de7224b79195bd0e1d/USER
@@ -132,6 +146,47 @@ ismc[30]=MC
 
 JobIndexList=${JobIndexList}" 30 "
 
+ds[40]=/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM
+name[40]=WjetIncl
+ismc[40]=MC
+
+JobIndexList=${JobIndexList}" 40 "
+
+
+ds[50]=/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v2/MINIAODSIM
+name[50]=ZjetIncl
+ismc[50]=MC
+
+ismc[51]=MC
+ismc[52]=MC
+ismc[53]=MC
+ismc[54]=MC
+ismc[55]=MC
+ismc[56]=MC
+ismc[57]=MC
+ismc[58]=MC
+
+name[51]=DYM50HT70to100
+name[52]=DYM50HT100to200
+name[53]=DYM50HT200to400
+name[54]=DYM50HT400to600
+name[55]=DYM50HT600to800
+name[56]=DYM50HT800to1200
+name[57]=DYM50HT1200to2500
+name[58]=DYM50HT2500toInf
+
+ds[51]=/DYJetsToLL_M-50_HT-70to100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM
+ds[52]=/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM
+ds[53]=/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM
+ds[54]=/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM
+ds[55]=/DYJetsToLL_M-50_HT-600to800_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v2/MINIAODSIM
+ds[56]=/DYJetsToLL_M-50_HT-800to1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM
+ds[57]=/DYJetsToLL_M-50_HT-1200to2500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM
+ds[58]=/DYJetsToLL_M-50_HT-2500toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM
+
+#JobIndexList=${JobIndexList}" 50 51 52 53 54 55 56 57 58 "
+JobIndexList=${JobIndexList}" 50 "
+
 # ds[21]=/TTTo2L2Nu_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM
 # name[21]=ttbar_dilep
 # ismc[21]=MC
@@ -159,10 +214,6 @@ JobIndexList=${JobIndexList}" 30 "
 #
 #        
 #
-#ds[25]=/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext1-v1/MINIAODSIM
-#name[25]=WjetIncl
-#ismc[25]=MC
-# 
 #ds[26]=/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext3-v1/MINIAODSIM
 #name[26]=ttbar
 #ismc[26]=MC
@@ -296,9 +347,23 @@ cat crabconfig_template.py | sed "s|XXXXX|${name[${idx}]}|g" | sed "s|YYYYY|${ni
 fi
 
 else
+
+# Data : 
+
+JsonFile=Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt
+if [ ${name[$idx]} == DataElF1 -o ${name[$idx]} == DataMuF1 ]
+then
+JsonFile=Cert_Morind17_uptoPeriodF1.txt
+fi
+if [ ${name[$idx]} == DataElF2 -o ${name[$idx]} == DataMuF2 ]
+then
+JsonFile=Cert_Morind17_uptoPeriodF2.txt
+fi
+
+
 cat crabconfig_template.py | sed "s|XXXXX|${name[${idx}]}|g" | sed "s|YYYYY|${nickname}|g" | sed "s|ZZZZZ|${ds[$idx]}|g" | \
                              sed "s|QQQQQ|${ismc[$idx]}|g" | \
-   sed "s|#PPPPP#|config.Data.lumiMask=\"../data/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt\"|g" \
+   sed "s|#PPPPP#|config.Data.lumiMask=\"../data/${JsonFile}\"|g" \
     > __CRABCONFIG__${name[${idx}]}.py
 fi
 

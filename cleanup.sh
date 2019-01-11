b79e1e42468869e9f9d64d63aa1db9410b720e36
Benjamin Tannenwald <benjamin.tannenwald@cern.ch>
2019-01-11 22:38:22 +0100

    udpated cleanup script. crab submission now works with BDT/MEM code

--- cleanup.sh
+++ cleanup.sh
@@ -6,3 +6,5 @@ rm -rf src/TTH/CommonClassifier/data/blrbdtweights_v1
 rm -rf src/TTH/CommonClassifier/data/bdtweights_Spring17V1
 rm -rf src/TTH/CommonClassifier/data/bdtweights_v5_74x
 rm -rf src/TTH/CommonClassifier/data/bdtweights_Spring17V3
+rm src/RecoEgamma/PhotonIdentification/data/MVA/Spring15/50ns_EB_V0.weights.xml.gz
+rm src/RecoEgamma/PhotonIdentification/data/MVA/Spring15/50ns_EE_V0.weights.xml.gz
